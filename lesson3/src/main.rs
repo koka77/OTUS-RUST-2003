4d7af4c0a3382487f4a66952e5eb9402d600bcbe
koka77 <debalast@ya.ru>
2023-12-13 19:45:27 +0300

    lesson3_2

--- lesson3/src/main.rs
+++ lesson3/src/main.rs
@@ -1,33 +1,45 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("{}", double_int32(2, 2));
+    println!("{}", double_int32(2, 2));
+    println!("{}", double_int64(3));
+    println!("{}", double_float32(4.));
+    println!("{}", double_float64(5));
+    println!("{}", int_plus_float_to_float(6, 7.));
+    println!("{}", int_plus_float_to_int(7, 8.));
+    println!("{}", tuple_sum((8, 9)));
+    println!("{}", array_sum([1,2,3]));
 }
 
-fn double_int32(x: i32) -> i32 {
+fn double_int32(x: u32, i: i32) -> u32 {
     x * 2
 }
 
-fn double_int64(x: i32) -> i64 {
-    (x * 2) as i64
+fn double_int64(x: u32) -> u64 {
+    (x * 2) as u64
 }
 
 fn double_float32(x: f32) -> f32 {
     x * 2.0
 }
 
-fn double_float64(x: i32) -> f64 {
+fn double_float64(x: u32) -> f64 {
     (x * 2) as f64
 }
 
-fn int_plus_float_to_float(x: i32, y: f32) -> f64 {
+fn int_plus_float_to_float(x: u32, y: f32) -> f64 {
     ((x as f32) + y) as f64
 }
 
-fn int_plus_float_to_int(x: i32, y: f32) -> i64 {
-    (x as f32 + y) as i64
+fn int_plus_float_to_int(x: u32, y: f32) -> u64 {
+    (x as f32 + y) as u64
 }
-fn tuple_sum((x, y): (i32, i32)) -> i64 {
-    (x + y) as i64
+
+fn tuple_sum((x, y): (u32, u32)) -> u64 {
+    (x + y) as u64
 }
-fn array_sum(arr: [i32; 3]) -> i32 {
-    arr[0] + arr[1] + arr[2]
+
+fn array_sum(arr: [u32; 3]) -> u64 {
+    (arr[0] + arr[1] + arr[2]) as u64
 }

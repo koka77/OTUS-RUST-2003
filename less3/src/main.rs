8b10341cb223e29ec2eab05fa951c7d651a0c3a5
koka77 <debalast@ya.ru>
2024-01-17 22:44:34 +0300

    hw_less6

--- less3/src/main.rs
+++ less3/src/main.rs
@@ -1,19 +1,3 @@
-struct Account {
-    balance: i64,
-}
-
-impl Drop for Account {
-    fn drop(&mut self) {
-        if self.balance != 0 {
-            println!("Attention! Balance is not zero")
-        } else {
-            println!("Account was delete")
-        }
-    }
-}
-
 fn main() {
-    let a1 = Account { balance: 0 };
-    // drop(a1);
-    let a2 = Account { balance: 42 };
+    println!("Hello, world!");
 }
